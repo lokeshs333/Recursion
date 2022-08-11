6af6ff75c6bcdfb7c913a3adb2494630cf756012
LOKESH PANDEY <73874665+lokeshs333@users.noreply.github.com>
2022-08-12 00:51:46 +0530

    Add files via upload

---
+++ chpter3.cpp
@@ -0,0 +1,21 @@
+// summision of first n numbers using recursion
+
+#include <bits/stdc++.h>
+using namespace std;
+void submission(int n, int sum)
+{
+    if (n == 0)
+    {
+        return;
+    }
+    sum = sum + n;
+    n--;
+    cout << "answer is " << sum;
+    submission(n, sum);
+}
+int main()
+{
+    submission(10, 0);
+    // cout << "your answer is " << ans << endl;
+    return 0;
+}
